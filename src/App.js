ef3655550ea6278f426fbb10b9c0f7768b822b21
Emery <abemery@clemson.edu>
2020-09-28 15:06:45 -0500

    does this fix deployment?

--- src/App.js
+++ src/App.js
@@ -14,6 +14,3 @@ export default () => {
     </Router>
   );
 };
-// const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-
-// const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
