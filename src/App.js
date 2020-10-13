1659d326a3c73f159fd2ed0aab330e7b4f7336fa
Emery <abemery@clemson.edu>
2020-10-13 12:58:11 -0400

    testing loading bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import './App.css';
-import Home from "./components/Home.js"
-import newRon from "./components/heyRon.js"
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navigation from './navigation'
+import Auth from '@aws-amplify/auth';
+
 
 export default () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/heyRon" component={newRon} />
-      </Switch>
-    </Router>
+    <Navigation />
   );
 };
