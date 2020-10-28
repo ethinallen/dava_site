3f7c3599d9391bbde6d5359c45220f1036aa0902
Emery <abemery@clemson.edu>
2020-10-28 13:31:57 -0400

    link to demo dash; trying opacity again but we'll see

---
+++ src/components/Demo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./Demo.css"
+import Button from 'react-bootstrap/Button';
+
+
+export default () => {
+  return (
+    <div id="buttonContainer">
+    Testing
+    </div>
+  );
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,35 +2,12 @@ import React from "react";
 import "./Home.css";
 import Particles from 'react-particles-js';
 import About from "./About.js";
+import Demo from "./Demo.js"
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
 
 export default () => {
   return (
-
-
     <body>
-      <Navbar sticky="top" bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
-
       <div id="particleContainer">
         <Particles
           params = {{
@@ -64,6 +41,7 @@ export default () => {
       <div id="bottomLeft"></div>
 
       <About />
+      <Demo />
     </body>
   );
 };
