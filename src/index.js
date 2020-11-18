accaf3d1dda98a24e8e88467f734d270a0d34f71
Emery <abemery@clemson.edu>
2020-11-17 19:41:23 -0500

    remove aws exports

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
 import "semantic-ui-css/semantic.min.css"
-
-Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
